a2b6bdaae650003d81d77a647eca1eb5aa2c3bdf
Elias Afara <afaraelias@gmail.com>
2023-10-13 13:28:01 +0200

    feat: added clerk authentication + routing

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <p className='text-3xl font-medium text-sky-700'>
-      This is a protected page
-    </p>
+    <div>
+      <UserButton afterSignOutUrl='/' />
+    </div>
   );
 }
