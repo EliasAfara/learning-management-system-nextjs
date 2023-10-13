a1af22ae6beb7826a845aeb29f15d319e0aa7eb9
Elias Afara <afaraelias@gmail.com>
2023-10-13 19:14:21 +0200

    created mobile navbar + navbar routes setup

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,9 +1,11 @@
-import MobileSidebar from "./mobile-sidebar";
+import NavbarRoutes from "@/components/navbar-routes";
+import { MobileSidebar } from "./mobile-sidebar";
 
 const Navbar = () => {
   return (
-    <div className='p-0 border-b h-full flex items-center bg-white shadow-sm'>
+    <div className='p-4 border-b h-full flex items-center bg-white shadow-sm'>
       <MobileSidebar />
+      <NavbarRoutes />
     </div>
   );
 };
